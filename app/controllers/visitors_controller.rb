26338aaa38b3c98b2f24b175c67b690937c39f84
RoR-Homer <ror-homer@gmx.de>
2015-03-09 08:40:01 -0700

    update application layout with gem

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -2,6 +2,8 @@ class VisitorsController < ApplicationController
 
 	def new
 		@owner = Owner.new
+		flash.now[:notice] = 'Welcome! Hans!'
+		flash.now[:alert] = 'My birthday is really soon!'
 	end
 
 end
