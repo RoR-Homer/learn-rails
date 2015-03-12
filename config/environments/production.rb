7a91e74fba1b64b98756ed6baf871bae9287c033
RoR-Homer <ror-homer@gmx.de>
2015-03-12 09:48:46 -0700

    BUGFIX

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,7 +89,7 @@ Rails.application.configure do
     domain: Rails.application.secrets.domain_name,
     authentication: "plain",
     enable_starttls_auto: true,
-    user_name: Rails.application.secrets.email_provider_username
+    user_name: Rails.application.secrets.email_provider_username,
     password: Rails.application.secrets.email_provider_password
   }
 
